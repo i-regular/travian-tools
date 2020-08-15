507b566de46825ad3d82d99534f77934ea8275b8
Andrej Polak <andrej.polak@saferpass.net>
2020-08-16 01:20:15 +0200

    Added distance calculation to PlayerForm component

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -10,10 +10,23 @@ interface IAppState {
   distances?: number[];
 }
 
-export class App extends React.PureComponent {
-  private attacker: IPlayerVillage = {
-    location: { x: 0, y: 0 },
-  };
+export class App extends React.Component {
+  private _getAttackerPlayerVillage(playerVillage: IPlayerVillage): void {
+    const newState = this.state;
+    newState.attackerVillage = playerVillage;
+    this.setState(newState);
+  }
+
+  private _getDefenderPlayerVillage(playerVillage: IPlayerVillage): void {
+    const newState = this.state;
+    if (newState.targetVillages === undefined) {
+      newState.targetVillages = [playerVillage];
+    } else {
+      newState.targetVillages.push(playerVillage);
+    }
+    this.setState(newState);
+  }
+
   public state: IAppState = {};
 
   public render(): JSX.Element {
@@ -22,10 +35,19 @@ export class App extends React.PureComponent {
         <h1>Hello Heathens, this will be the best planning tool there is</h1>
         <ControlGroup fill={false} vertical={true}>
           <ControlGroup fill={false} vertical={false}>
-            <PlayerForm name="Attacker village"></PlayerForm>
+            <PlayerForm
+              name="Attacker village"
+              id="attacker"
+              sendPlayerVillage={this._getAttackerPlayerVillage.bind(this)}
+            ></PlayerForm>
           </ControlGroup>
           <ControlGroup fill={false} vertical={false}>
-            <PlayerForm name="Target village"></PlayerForm>
+            <PlayerForm
+              name="Target village"
+              id="target"
+              sendPlayerVillage={this._getDefenderPlayerVillage.bind(this)}
+              attacker={this.state.attackerVillage}
+            ></PlayerForm>
           </ControlGroup>
         </ControlGroup>
       </div>
--- frontend/components/Player-form.tsx
+++ frontend/components/Player-form.tsx
@@ -1,10 +1,13 @@
 import { ControlGroup, InputGroup, Label } from "@blueprintjs/core";
 import React from "react";
 import { parseMapLocationFromVillageLink } from "../src/parse-map-location";
-import { IPlayerVillage } from "../src/calculate-distance";
+import { IPlayerVillage, calculateDistance } from "../src/calculate-distance";
 
 interface IPlayerFormProps {
   name: string;
+  id: string;
+  sendPlayerVillage(playerVillage: IPlayerVillage): void;
+  attacker?: IPlayerVillage;
 }
 
 export class PlayerForm extends React.Component<IPlayerFormProps> {
@@ -19,13 +22,26 @@ export class PlayerForm extends React.Component<IPlayerFormProps> {
     },
   };
 
+  private _distanceInput: JSX.Element | null = null;
+
   private setVillageLocation = () => {
-    const linkElement = document.querySelector("#link") as HTMLInputElement;
+    const linkElement = document.querySelector(
+      `#${this.props.id}`,
+    ) as HTMLInputElement;
     const villageLocation = parseMapLocationFromVillageLink(linkElement.value);
     console.log(villageLocation);
 
     if (villageLocation !== null) {
+      this.props.sendPlayerVillage({ location: villageLocation });
       this.setState({ location: villageLocation });
+      if (this.props.attacker) {
+        const distance = calculateDistance(this.props.attacker, {
+          location: villageLocation,
+        });
+        this._distanceInput = (
+          <InputGroup value={`${distance}`} readOnly={true} />
+        );
+      }
     }
   };
 
@@ -34,24 +50,25 @@ export class PlayerForm extends React.Component<IPlayerFormProps> {
       <ControlGroup fill={false} vertical={false}>
         <Label htmlFor="link">{this.props.name}</Label>
         <InputGroup
-          id="link"
-          placeholder="Attacker link"
+          id={this.props.id}
+          placeholder="Village link"
           onChange={this.setVillageLocation}
         />
         <Label htmlFor="x">X:</Label>
         <InputGroup
           id="x"
           placeholder="x"
-          disabled={true}
+          readOnly={true}
           value={`${this.state.location.x}`}
         />
         <Label htmlFor="y">Y:</Label>
         <InputGroup
           id="y"
           placeholder="y"
-          disabled={true}
+          readOnly={true}
           value={`${this.state.location.y}`}
         />
+        {this._distanceInput}
       </ControlGroup>
     );
   }
