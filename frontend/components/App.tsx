88d8147f3cd4f1247c04bd41a8fc776180d2c474
Andrej Polak <andrej.polak@saferpass.net>
2020-08-20 19:45:53 +0200

    Added buttons and logig for adding attackers and targets

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-
 import { PlayerForm } from "./Player-form";
-import { ControlGroup } from "@blueprintjs/core";
+import { ControlGroup, FormGroup, InputGroup } from "@blueprintjs/core";
 import { IPlayerVillage } from "../src/calculate-distance";
-
+interface Attacker {
+  targetVillages: string[];
+}
+interface Attackers {
+  [id: string]: Attacker;
+}
 interface IAppState {
   attackerVillage?: IPlayerVillage;
   targetVillages?: IPlayerVillage[];
   distances?: number[];
+  attackers: Attackers;
 }
-
-export class App extends React.Component {
+export class App extends React.Component<any, IAppState> {
+  state: IAppState = { attackers: {} };
   private _getAttackerPlayerVillage(playerVillage: IPlayerVillage): void {
     const newState = this.state;
     newState.attackerVillage = playerVillage;
     this.setState(newState);
   }
-
-  private _getDefenderPlayerVillage(playerVillage: IPlayerVillage): void {
+  _getDefenderPlayerVillage = (playerVillage: IPlayerVillage) => {
     const newState = this.state;
     if (newState.targetVillages === undefined) {
       newState.targetVillages = [playerVillage];
@@ -25,31 +29,71 @@ export class App extends React.Component {
       newState.targetVillages.push(playerVillage);
     }
     this.setState(newState);
-  }
-
-  public state: IAppState = {};
-
+  };
+  addAttacker = () => {
+    const newAttacker = `${Date.now()}`;
+    this.setState({
+      attackers: {
+        ...this.state.attackers,
+        [newAttacker]: { targetVillages: [] },
+      },
+    });
+  };
+  addTarget = (attackerId: string) => {
+    this.setState({
+      attackers: {
+        ...this.state.attackers,
+        [attackerId]: {
+          targetVillages: [
+            ...this.state.attackers[attackerId].targetVillages,
+            "",
+          ],
+        },
+      },
+    });
+  };
   public render(): JSX.Element {
     return (
       <div>
         <h1>Hello Heathens, this will be the best planning tool there is</h1>
-        <ControlGroup fill={false} vertical={true}>
-          <ControlGroup fill={false} vertical={false}>
-            <PlayerForm
-              name="Attacker village"
-              id="attacker"
-              sendPlayerVillage={this._getAttackerPlayerVillage.bind(this)}
-            ></PlayerForm>
-          </ControlGroup>
-          <ControlGroup fill={false} vertical={false}>
-            <PlayerForm
-              name="Target village"
-              id="target"
-              sendPlayerVillage={this._getDefenderPlayerVillage.bind(this)}
-              attacker={this.state.attackerVillage}
-            ></PlayerForm>
-          </ControlGroup>
-        </ControlGroup>
+        <button type="button" onClick={this.addAttacker}>
+          Add attacker
+        </button>
+        {Object.keys(this.state.attackers).map((attacker) => {
+          return (
+            <ControlGroup fill={false} vertical={true}>
+              <ControlGroup fill={false} vertical={false}>
+                <PlayerForm
+                  name={attacker}
+                  id={attacker}
+                  sendPlayerVillage={this._getAttackerPlayerVillage.bind(this)}
+                />
+              </ControlGroup>
+              {this.state.attackers[attacker].targetVillages &&
+                this.state.attackers[attacker].targetVillages?.map(
+                  (targetVillage) => {
+                    return (
+                      <ControlGroup fill={false} vertical={false}>
+                        <PlayerForm
+                          name={targetVillage}
+                          id={targetVillage}
+                          sendPlayerVillage={this._getDefenderPlayerVillage}
+                          attacker={this.state.attackerVillage}
+                        />
+                      </ControlGroup>
+                    );
+                  },
+                )}
+              <button
+                onClick={() => {
+                  this.addTarget(attacker);
+                }}
+              >
+                Add target
+              </button>
+            </ControlGroup>
+          );
+        })}
       </div>
     );
   }
--- frontend/components/Player-form.tsx
+++ frontend/components/Player-form.tsx
@@ -2,58 +2,56 @@ import { ControlGroup, InputGroup, Label } from "@blueprintjs/core";
 import React from "react";
 import { parseMapLocationFromVillageLink } from "../src/parse-map-location";
 import { IPlayerVillage, calculateDistance } from "../src/calculate-distance";
-
 interface IPlayerFormProps {
   name: string;
   id: string;
   sendPlayerVillage(playerVillage: IPlayerVillage): void;
   attacker?: IPlayerVillage;
 }
-
 export class PlayerForm extends React.Component<IPlayerFormProps> {
+  private villageLinkInputRef: React.RefObject<HTMLInputElement>;
   constructor(props: IPlayerFormProps) {
     super(props);
+    this.villageLinkInputRef = React.createRef<HTMLInputElement>();
   }
-
   public state: IPlayerVillage = {
+    // locations:
     location: {
       x: 0,
       y: 0,
     },
   };
-
   private _distanceInput: JSX.Element | null = null;
-
   private setVillageLocation = () => {
-    const linkElement = document.querySelector(
-      `#${this.props.id}`,
-    ) as HTMLInputElement;
-    const villageLocation = parseMapLocationFromVillageLink(linkElement.value);
-    console.log(villageLocation);
+    const villageInput = this.villageLinkInputRef.current;
+    if (villageInput) {
+      const villageLocation = parseMapLocationFromVillageLink(villageInput.value);
+      console.log(villageLocation);
 
-    if (villageLocation !== null) {
-      this.props.sendPlayerVillage({ location: villageLocation });
-      this.setState({ location: villageLocation });
-      if (this.props.attacker) {
-        const distance = calculateDistance(this.props.attacker, {
-          location: villageLocation,
-        });
-        this._distanceInput = (
-          <InputGroup value={`${distance}`} readOnly={true} />
-        );
+      if (villageLocation !== null) {
+        this.props.sendPlayerVillage({ location: villageLocation });
+        this.setState({ location: villageLocation });
+        if (this.props.attacker) {
+          const distance = calculateDistance(this.props.attacker, {
+            location: villageLocation,
+          });
+          this._distanceInput = (
+            <InputGroup value={`${distance}`} readOnly={true} />
+          );
+        }
       }
     }
   };
-
   public render(): JSX.Element {
     return (
       <ControlGroup fill={false} vertical={false}>
         <Label htmlFor="link">{this.props.name}</Label>
-        <InputGroup
-          id={this.props.id}
-          placeholder="Village link"
-          onChange={this.setVillageLocation}
-        />
+          <input
+            ref={this.villageLinkInputRef}
+            id={this.props.id}
+            placeholder="Village link"
+            onChange={this.setVillageLocation}
+          />
         <Label htmlFor="x">X:</Label>
         <InputGroup
           id="x"
